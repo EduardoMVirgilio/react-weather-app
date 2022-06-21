66d00e39520db27bea9fcbc91b713e32eb45b70f
EduardoMVirgilio <eduardomiguelvirgilio@gmail.com>
2022-06-21 14:56:21 -0300

    Update Selector

--- src/components/selector.js
+++ src/components/selector.js
@@ -1,11 +1,19 @@
-import {useContext} from 'react';
+import {useContext,useState} from 'react';
 import {countryContext} from '../context/country';
 function Selector() {
 const context = useContext(countryContext);
+const [region, setRegion] = useState('america');
 const {country,setCountry} = context;
  return (
     <>
         <h1>Country Selector : {country}</h1>
+        <select onChange={(e) => setRegion(e.target.value)}>
+            <option value="america">America</option>
+            <option value="europe">Europe</option>
+            <option value="asia">Asia</option>
+            <option value="oceania">Oceania</option>
+            <option value="africa">Africa</option>
+        </select>
         <select onChange={(e) => setCountry(e.target.value)}>
             <option value="argentina">Argentina</option>
             <option value="brazil">Brazil</option>
